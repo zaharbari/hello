163188dbe4cc87492a123085cb5df57e9d01dcf5
zaharbari <zbari2@huskers.unl.edu>
2023-08-24 10:39:42 -0700

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ *  Author: Zahar Bari and Eleanor Krause
+ * Date: 2023/08/24
+ *
+ * A program that prints your name
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+int main(int argc, char **argv) {
+
+   printf("Zahar Bari and Eleanor Krause!\n");
+
+   return 0;
+}
